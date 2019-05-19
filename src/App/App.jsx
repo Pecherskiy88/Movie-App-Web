d2d40af7596a17308f23b292696883611b8349b1
Pecherskiy88 <pecherskiy88@gmail.com>
2019-05-20 00:55:09 +0300

    add calendar component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { asyncData } from "../redux/action/fetchAction";
+import Datepicker from "../Calendar/Calendar";
 // import s from "./App.module.css";
 
 class App extends Component {
+  state = {
+    date: new Date()
+  };
   componentDidMount() {
     this.props.fetch("2019", "05", "21");
   }
   render() {
-    return <div />;
+    const { date } = this.state;
+    return (
+      <div>
+        <Datepicker value={date} />
+      </div>
+    );
   }
 }
 const mapDispathToProps = dispatch => ({
