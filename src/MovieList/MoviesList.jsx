ec4b44f0ddd77765f0ff86d6f5bb708a83ec2d40
Pecherskiy88 <pecherskiy88@gmail.com>
2019-05-20 23:58:43 +0300

    current date from click done

--- src/MovieList/MoviesList.jsx
+++ src/MovieList/MoviesList.jsx
@@ -5,15 +5,24 @@ import { asyncData } from "../redux/action/fetchAction";
 class MovieList extends Component {
   componentDidMount() {
     console.log("MovieDate: ", this.props.dateValue);
-    let date = this.props.dateValue;
-    this.props.fetch();
+    let fullDate = this.props.dateValue;
+
+    let year = fullDate.getFullYear();
+    let month =
+      fullDate.getMonth() + 1 < 10
+        ? "0" + fullDate.getMonth()
+        : fullDate.getMonth();
+    let day =
+      fullDate.getDate() < 10 ? "0" + fullDate.getDate() : fullDate.getDate;
+
+    this.props.fetch(year, month, day);
   }
   render() {
     return <div>MovieList</div>;
   }
 }
 const mapDispatchToProps = dispatch => ({
-  fetch: () => dispatch(asyncData("2019", "05", "30"))
+  fetch: (year, month, day) => dispatch(asyncData(year, month, day))
 });
 
 export default connect(
